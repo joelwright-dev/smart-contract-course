dc35d09077256135d0d8c18fad1c5c3fb07a4532
joelwright-dev <proxy.coding@gmail.com>
2022-11-27 06:36:28 +1000

    Hard Hat config, deployment script, and tests created

--- Hard Hat/Hard Hat Simple Storage/hardhat.config.js
+++ Hard Hat/Hard Hat Simple Storage/hardhat.config.js
@@ -1,6 +1,33 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+	defaultNetwork: "hardhat",
+	networks: {
+		goerli: {
+			url: process.env.GOERLI_RPC_URL,
+			accounts: [process.env.PRIVATE_KEY],
+			chainId: 5,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			chainId: 31337,
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
+	gasReporter: {
+		enabled: true,
+		outputFile: "gas-report.txt",
+		noColors: true,
+		currency: "USD",
+		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+	},
+	solidity: "0.8.17",
+}
