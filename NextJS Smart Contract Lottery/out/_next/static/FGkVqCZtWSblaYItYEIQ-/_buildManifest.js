826746beefa44e317a0404e2e2d6432284aac69e
joelwright-dev <proxy.coding@gmail.com>
2022-12-05 19:28:39 +1000

    Removed out from .gitignore

---
+++ NextJS Smart Contract Lottery/out/_next/static/FGkVqCZtWSblaYItYEIQ-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/567-c18937b29cca7869.js","static/css/ae0e3e027412e072.css","static/chunks/pages/index-4d6c9a3e0ad7ec91.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ NextJS Smart Contract Lottery/out/_next/static/chunks/744.982be63282e36e72.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[744],{67668:function(){},63866:function(){}}]);
