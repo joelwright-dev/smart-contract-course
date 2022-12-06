a8d914d322e5bf410545e3f481dbb843f6e9226a
joelwright-dev <proxy.coding@gmail.com>
2022-12-06 19:38:13 +1000

    Added functions to deposit collateral (ETH/wETH)

---
+++ Hardhat DeFi/scripts/aaveBorrow.js
@@ -0,0 +1,57 @@
+const { getWeth, AMOUNT } = require("./getWeth")
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+	await getWeth()
+	const { deployer } = await getNamedAccounts()
+
+	//Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+	const lendingPool = await getLendingPool(deployer)
+	console.log(`LendingPool address: ${lendingPool.address}`)
+
+	const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
+	await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+	console.log("Depositing...")
+	await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+	console.log("Deposited!")
+}
+
+async function getLendingPool(account) {
+	const lendingPoolAddressesProvider = await ethers.getContractAt(
+		"ILendingPoolAddressesProvider",
+		"0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+		account
+	)
+	const lendingPoolAddress =
+		await lendingPoolAddressesProvider.getLendingPool()
+	const lendingPool = await ethers.getContractAt(
+		"ILendingPool",
+		lendingPoolAddress,
+		account
+	)
+	return lendingPool
+}
+
+async function approveErc20(
+	erc20Address,
+	spenderAddress,
+	amountToSpend,
+	account
+) {
+	const erc20Token = await ethers.getContractAt(
+		"IERC20",
+		erc20Address,
+		account
+	)
+	const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+	await tx.wait(1)
+	console.log("Approved!")
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.log(error)
+		process.exit(1)
+	})
--- Hardhat ERC20/contracts/OurToken.sol
+++ Hardhat ERC20/contracts/OurToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
-	constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+	constructor(uint256 initialSupply) ERC20("Our Token", "OT") {
 		_mint(msg.sender, initialSupply);
 	}
 }
